2547c5650ea88240942b2e211d4bee4675aa8624
Shinichi Okada <okada.shin@gmail.com>
2023-02-11 10:27:31 +0900

    version v0.5.6

--- consize.rb
+++ consize.rb
@@ -1,13 +1,14 @@
 class Consize < Formula
     desc "Consize converts and resize JPEG and PNG to WEBP format."
     homepage "https://consize.codewithshin.com"
-    url "https://github.com/shinokada/consize/archive/refs/tags/v0.5.4.tar.gz"
-    sha256 "4739168eb148b0f48d18c0aba664f8e99f4de50f8d23f1c0c328e29e1ca2c35d"
+    url "https://github.com/shinokada/consize/archive/refs/tags/v0.5.6.tar.gz"
+    sha256 "c57a25561ef71104c99eaa26fde2945b327c0c210fbaf527ef443b1737f03ae6"
     license "MIT"
   
     depends_on "imagemagick"
     depends_on "webp"
     depends_on "optipng"
+    depends_on "jpegoptim"
 
   
     def install
